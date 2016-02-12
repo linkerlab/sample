07ad8090326b6d211cc0ad5d33c210861cdb913c
Simon Su <simonsu.mail@gmail.com>
2016-02-12 05:07:39 +0000

    test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ const port = 1337;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World Simon ~\n');
+  res.end('Hello World Simon Su~\n');
 }).listen(port, () => {
   console.log(`Server running at http://your-ip-address:${port}/`);
 });
